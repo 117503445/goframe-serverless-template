d7fb6f3c6199afc94d3ae2cc3aee561edba12e8a
117503445 <t117503445@gmail.com>
2022-01-26 16:47:34 +0000

    feat: 1.4

--- internal/controller/hello.go
+++ internal/controller/hello.go
@@ -17,7 +17,7 @@ type cHello struct{}
 func (h *cHello) Hello(ctx context.Context, req *apiv1.HelloReq) (res *apiv1.HelloRes, err error) {
 	g.Log().Line(true).Debug(ctx, "Hello")
 	res = &apiv1.HelloRes{
-		Version: "1.3",
+		Version: "1.4",
 	}
 	return
 }
