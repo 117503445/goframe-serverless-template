728e7215e8c45365d360fe9a626a98e0400768a3
117503445 <t117503445@gmail.com>
2022-01-26 15:41:56 +0000

    feat: print dblink

--- internal/controller/hello.go
+++ internal/controller/hello.go
@@ -6,6 +6,7 @@ import (
 	"goframe-serverless-template/apiv1"
 
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/genv"
 )
 
 var (
@@ -16,6 +17,8 @@ type cHello struct{}
 
 func (h *cHello) Hello(ctx context.Context, req *apiv1.HelloReq) (res *apiv1.HelloRes, err error) {
 	g.Log().Line(true).Debug(ctx, "Hello")
+	dblink := genv.GetWithCmd("dblink").String()
+	g.Log().Line().Debug(ctx, "dblink = ", dblink)
 	res = &apiv1.HelloRes{
 		Version: "1.0",
 	}
